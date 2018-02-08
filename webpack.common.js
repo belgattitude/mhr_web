8ad80fb2f6f7c6f2c3a5bf28e315882f8531a4f1
belgattitude <belgattitude@gmail.com>
2018-02-08 14:08:51 +0100

    refactored webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
 
-//const AddModuleExportsPlugin = require('add-module-exports-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const extractSass = new ExtractTextPlugin({
-  filename: "style.css",
-});
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
-  //devtool: 'eval',
   entry: [
     'babel-polyfill',
     './src/js/index.tsx'
@@ -22,18 +13,12 @@ module.exports = {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     modules: [
       path.join(__dirname, 'src'),
-      'node_modules',
+      'node_modules'
     ],
     alias: {
-      // https://medium.com/@martin_hotell/type-safe-es2015-module-import-path-aliasing-with-webpack-typescript-and-jest-fe461347e010
       '@src': path.resolve(__dirname, 'src/js/'),
       '@public': path.resolve(__dirname, 'public/')
     }
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].js',
-    publicPath: '/', // for production it would be './'
   },
   module: {
     rules: [
@@ -42,8 +27,7 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'babel-loader',
-            //options: babelOptions
+            loader: 'babel-loader'
           },
           {
             loader: 'awesome-typescript-loader'
@@ -58,38 +42,20 @@ module.exports = {
           options: {
             cacheDirectory: true
           }
-        },
-      },
-
-      {
-        test: /\.(scss)$/,
-        /** remove css-hot-loader for production */
-        use: ['css-hot-loader'].concat(extractSass.extract({
-          fallback: 'style-loader',
-          //resolve-url-loader may be chained before sass-loader if necessary
-          use: [{
-            loader: "css-loader" // translates CSS into CommonJS
-          }, {
-            loader: "sass-loader" // compiles Sass to CSS
-          }]
-        }))
-      },
-      {
+        }
+      },{
         test: /\.(glsl|vs|fs)$/,
         loader: 'ts-shader-loader'
-      },
-      {
+      },{
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(mp4|m4v)$/,
+      },{
+        test: /\.(mp4|m4v|ogv|webm)$/,
         loader: 'file-loader',
         options: {
           name: 'assets/videos/[name].[ext]',
         }
-      },
-      {
+      },{
         test: /\.woff$|\.woff2?$|\.ttf$|\.eot$|\.otf$/,
         loader: 'file-loader',
         //use: 'url-loader?limit=10000',
@@ -100,29 +66,19 @@ module.exports = {
             return url.replace(/public/, '..')
           },*/
         },
-      },
-      {
+      },{
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           'file-loader?name=images/[name].[ext]',
           'image-webpack-loader?bypassOnDebug'
         ]
       }
-
     ]
   },
   plugins: [
-/*
-    new webpack.EnvironmentPlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-      NODE_ENV: 'production'
-    }),
-
-    new UglifyJSPlugin({
-      parallel: true,
-      sourceMap: true
-    }),
-*/
+    new webpack.EnvironmentPlugin([
+      'NODE_ENV',
+    ]),
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
       hash: true,
@@ -136,17 +92,6 @@ module.exports = {
       $: "jquery", // Used for Bootstrap JavaScript components
       jQuery: "jquery", // Used for Bootstrap JavaScript components
       Popper: ['popper.js', 'default'] // Used for Bootstrap dropdown, popup and tooltip JavaScript components
-    }),
-    extractSass,
-
-  ],
-  devServer: {
-    contentBase: path.resolve(__dirname, "build"),
-    port: 3001,
-    historyApiFallback: true,
-    hot: true,
-    headers: {
-        'Access-Control-Allow-Origin': '*',
-    }
-  }
+    })
+  ]
 };
---
+++ webpack.prod.js
@@ -0,0 +1,89 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const common = require('./webpack.common.js');
+
+const extractSass = new ExtractTextPlugin({
+  filename: "style.css",
+});
+
+getMinifyPlugin = function(version) {
+  var minPlugin = '';
+  switch (version) {
+    case 'uglify_v1':
+      const UglifyJsPlugin1 = require('uglifyjs-webpack-plugin');
+      minPlugin = new UglifyJsPlugin1({
+        parallel: true,
+        sourceMap: true,
+        uglifyOptions: {
+          comments: false,
+          ecma: 5,
+          compress: true,
+          warnings: true
+        }
+      });
+      break;
+
+    default:
+      // Bundled webpack uglify
+      minPlugin = new webpack.optimize.UglifyJsPlugin({
+        parallel: true,
+        sourceMap: true,
+        compress: {
+          warnings: true
+        },
+        output: {
+          comments: false
+        }
+      });
+  }
+  return minPlugin;
+};
+
+
+module.exports = merge(common, {
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: extractSass.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: [{
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      NODE_ENV: JSON.stringify('production')
+    }),
+
+
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+
+    getMinifyPlugin('uglify_v1'),
+
+    extractSass,
+
+  ],
+
+  devtool: false
+});
+
+
