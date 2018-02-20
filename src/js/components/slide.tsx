b3f56a1526f21841bea584c2363a1df092423c3f
belgattitude <belgattitude@gmail.com>
2018-02-20 15:38:10 +0100

    Updated

---
+++ src/js/components/slide.tsx
@@ -0,0 +1,153 @@
+import React from 'react';
+import SplitText from '@src/thirdparty/SplitText.min.js';
+
+import {TimelineLite, TweenLite, Back, Elastic} from 'gsap';
+import {Transition} from "react-transition-group";
+
+class Slide extends React.Component<{ text: string, image: string, in?: boolean }, {}> {
+    protected domRef: HTMLDivElement;
+    protected tl: TimelineLite;
+    protected splitText: {
+        chars: string[],
+        words: string[],
+    };
+
+    componentDidMount() {
+        this.tl = new TimelineLite({paused: true});
+        /*
+        TweenLite.set(this.domRef, {
+            css: {
+                perspective: 500,
+                perspectiveOrigin: "50% 50%",
+                transformStyle: "preserve-3d"
+            }
+        });
+        */
+
+        const textTarget = this.domRef.querySelector("div");
+        // todo need to test null
+        const imgTarget = (this.domRef.querySelector("img") as HTMLImageElement);
+
+        TweenLite.set(textTarget, {
+            transformPerspective: 600,
+            perspective: 300,
+            transformStyle: "preserve-3d",
+            autoAlpha: 1
+        });
+
+
+        this.splitText = new SplitText(textTarget, {type: "chars"});
+
+        const numChars = this.splitText.chars.length;
+
+        const getRandomInt = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        const groupCharsDuration = 0.8;
+
+        for (let i = 0; i < numChars; i++) {
+            this.tl.from(
+                this.splitText.chars[i],
+                groupCharsDuration,
+                {
+                    css: {
+                        y: getRandomInt(-75, 75),
+                        x: getRandomInt(-150, 150),
+                        rotation: getRandomInt(0, 720),
+                        autoAlpha: 0
+                    }, ease: Back.easeOut
+                },
+                i * 0.02
+            );
+        }
+
+        this.tl.add('end_of_group_chars', groupCharsDuration);
+
+        this.tl.from(imgTarget, 3, {
+            css: {
+                //scale: 5,
+                //rotation: 0,
+                opacity: 0
+                //filter:
+            },
+            filter:"blur(0px}",
+            //ease: Back.easeInOut
+        }, 0)
+
+        /*
+        TweenMax.to(this.domRef, 0.2, {
+            textShadow:"0px 0px 15px white",
+            color:"none" // IE10 unfortunately hides the shadow too
+        });
+        */
+
+        this.tl.staggerTo(this.splitText.chars, 3, {
+            css: {
+                transformOrigin: "50% 50% -30px",
+                rotationY: -360,
+                rotationX: 360,
+                rotation: 360
+            }, ease: Elastic.easeInOut
+        }, 0.02, "+=1");
+
+    }
+
+    componentWillUnmount() {
+        console.log('component will unmount');
+    }
+
+    endListener = (node: HTMLElement, done: () => void) => {
+        console.log('endListener', this.props.in);
+        if (this.props.in) {
+            this.tl.play(0).eventCallback('onComplete', () => {
+                console.log('starting', 'onComplete')
+                done();
+            });
+        } else {
+            this.tl.reverse(0.8)
+                .timeScale(1.5)
+                .eventCallback('onReverseComplete', () => {
+                    console.log('removing', 'onReverseComplete')
+                    done();
+                    delete this.domRef;
+                });
+        }
+    }
+
+    render() {
+        const lines = this.props.text.split("\n");
+
+        return (
+            <Transition
+                in={this.props.in}
+                timeout={2000}
+                mountOnEnter={true}
+                unmountOnExit={true}
+                addEndListener={this.endListener}
+            >
+                {state => {
+                    return (
+                        <div ref={(el: HTMLDivElement) => {
+                            this.domRef = el
+                        }}>
+                            <div>
+                                {lines.map((line, idx) => (
+                                    <React.Fragment key={idx}>
+                                        <p>{line.trim()}</p>
+                                    </React.Fragment>
+                                ))}
+                            </div>
+                            <img className="test" src={this.props.image} />
+                        </div>
+                    )
+                }
+                }
+            </Transition>
+        )
+    }
+
+}
+
+
+export default Slide;
--- src/js/containers/menu-layout.tsx
+++ src/js/containers/menu-layout.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import SplitText from '@src/thirdparty/SplitText.min.js';
-
+import Slide from "@src/components/slide";
+import {TransitionGroup} from "react-transition-group";
 import './menu-layout.scss';
-import {TimelineLite, TweenLite, Back, Elastic, Power2} from 'gsap';
-import {Transition, TransitionGroup} from "react-transition-group";
-
 
 interface IProps {
     title: string;
@@ -17,12 +14,10 @@ interface IState {
 
 class MenuLayout extends React.Component<IProps, IState> {
 
-
     state = {
         showSlide: false,
         count: 1
     };
-
 
     constructor(props: IProps) {
         super(props);
@@ -33,36 +28,28 @@ class MenuLayout extends React.Component<IProps, IState> {
 
     }
 
-    toggleSlide(e: any) {
-
-        this.setState((prevState, props) => {
-            return {...prevState, showSlide: !prevState.showSlide};
-        });
-
-    }
-
     protected getPathToImages() {
         return require.context('@assets/images', true, /\.jpg$/);
     }
-
 
 
     render() {
         const pathToImages = this.getPathToImages();
         const imageUrl = pathToImages('./texture_bg.jpg');
 
-        const text = `
-                            From the edges of a new born idea.
-                            I'll find a way to
-                            get inside.
-                            Move on
-        `;
+        const texts = [
+            "From the edges of a new born idea.\nI'll find a way to get inside.",
+            "Behind the curtains, under a dress\nA clowny smile on my lips",
+            "Forbidden kisses.\nUnder an ocean of salt.\nSpices",
+            "On his way to how.\nA clever idea of who we are\ndespite te appearances.",
+        ]
+
+        const text = texts[(this.state.count % texts.length)];
 
         //const video = 'http://localhost/paola/trailer_hd.mp4';
-        const showSlide = this.state.showSlide;
 
-        const elements = [
-            <FadeEl key="cool" content="Hello" target="1" remove={() => { console.log('remove')}} />
+        let elements: JSX.Element[] = [
+           /* <FadeEl key="cool" content="Hello" target="1" remove={() => { console.log('remove')}} />*/
         ];
 
         const images = 'wxqlQkh,G2Whuq3,0bUSEBX'
@@ -71,11 +58,17 @@ class MenuLayout extends React.Component<IProps, IState> {
 
         const image = images[this.state.count % 3];
 
+        const newSlide = <Slide text={text} image={image} key={`slide-${this.state.count}`} />;
 
-        if (this.state.count % 2 == 0) {
-            elements.push(<Slide text={text} image={image} key={`slide-${this.state.count}`} />);
-        }
+        //if (this.state.count > 1) {
+            elements.push(newSlide);
+        //}
 
+        //if (this.state.count % 2 == 0) {
+            //elements.push(<Slide text={text} image={image} key={`slide-${this.state.count}`} />);
+        //}
+
+        console.log('elements', elements);
 
         return (
             <div className="main-wrapper">
@@ -84,19 +77,11 @@ class MenuLayout extends React.Component<IProps, IState> {
                     <div className="layer">
 
                         <div className="title">
-                            {(showSlide) ?
                                 <TransitionGroup>
                                     {elements.map(elem => (elem))}
                                 </TransitionGroup>
-                                :
-                                <TitleBox text={text}/>
-
-                            }
                         </div>
-
-
                         <div className="timeline">
-                            <button onClick={(e:any) => {this.toggleSlide(e);}}>Cool</button>
                             <button onClick={(e:any) => {
                                 this.setState((prevState, props) => (
                                     {...prevState, count: prevState.count + 1}
@@ -111,279 +96,6 @@ class MenuLayout extends React.Component<IProps, IState> {
     }
 }
 
-class TitleBox extends React.Component<{ text: string }, {}> {
 
-    protected domRef: HTMLDivElement;
-    protected tl: TimelineLite;
-
-    componentDidMount() {
-        //console.log('component did mount');
-        this.tl = new TimelineLite();
-        const mySplitText = new SplitText(this.domRef, {type: "chars"});
-
-        TweenLite.set(this.domRef, {
-            css: {
-                perspective: 500,
-                perspectiveOrigin: "50% 50%",
-                transformStyle: "preserve-3d"
-            }
-        });
-
-        const numChars = mySplitText.chars.length;
-
-        const getRandomInt = (min, max) => {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-        for (let i = 0; i < numChars; i++) {
-            (this.tl as any).from(mySplitText.chars[i], 0.8,
-                {
-                    css: {
-                        y: getRandomInt(-75, 75),
-                        x: getRandomInt(-150, 150),
-                        rotation: getRandomInt(0, 720),
-                        autoAlpha: 0
-                    }, ease: Back.easeOut
-                }, i * 0.02
-                , "dropIn"
-            );
-        }
-
-        this.tl.staggerTo(mySplitText.chars, 4, {
-            css: {
-                transformOrigin: "50% 50% -30px",
-                rotationY: -360,
-                rotationX: 360,
-                rotation: 360
-            }, ease: Elastic.easeInOut
-        }, 0.02, "+=1");
-
-
-        TweenLite.set(this.domRef, {
-            transformPerspective: 600,
-            perspective: 300,
-            transformStyle: "preserve-3d",
-            autoAlpha: 1
-        });
-
-        // slowering the thing
-        //this.tl.timeScale(0.2);
-        this.tl.play();
-        //tl.restart();
-
-    }
-
-    render() {
-        const lines = this.props.text.split("\n");
-        return (
-            <div ref={(el: HTMLDivElement) => {
-                this.domRef = el
-            }}>
-                {lines.map((line, idx) => (
-                    <React.Fragment key={idx}>
-                        <p>{line.trim()}</p>
-                    </React.Fragment>
-
-                ))}
-            </div>
-        )
-    }
-
-}
-
-
-/*
-const defaultStyle = {
-    opacity: 1,
-    transform: "translate(300px, 0)"
-};*/
-
-
-class Slide extends React.Component<{ text: string, image: string, in?: boolean }, {}> {
-
-    protected domRef: HTMLDivElement;
-    protected tl: TimelineLite;
-    protected splitText: {
-        chars: string[],
-        words: string[],
-    };
-
-    componentDidMount() {
-        this.tl = new TimelineLite({paused: true});
-        /*
-        TweenLite.set(this.domRef, {
-            css: {
-                perspective: 500,
-                perspectiveOrigin: "50% 50%",
-                transformStyle: "preserve-3d"
-            }
-        });
-        */
-
-        TweenLite.set(this.domRef, {
-            transformPerspective: 600,
-            perspective: 300,
-            transformStyle: "preserve-3d",
-            autoAlpha: 1
-        });
-
-
-        this.splitText = new SplitText(this.domRef, {type: "chars"});
-
-        const numChars = this.splitText.chars.length;
-
-        const getRandomInt = (min, max) => {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-
-        for (let i = 0; i < numChars; i++) {
-            (this.tl as any).from(this.splitText.chars[i], 0.8,
-                {
-                    css: {
-                        y: getRandomInt(-75, 75),
-                        x: getRandomInt(-150, 150),
-                        rotation: getRandomInt(0, 720),
-                        autoAlpha: 0
-                    }, ease: Back.easeOut
-                }, i * 0.02
-                , "dropIn"
-            );
-        }
-
-
-        this.tl.staggerTo(this.splitText.chars, 3, {
-            css: {
-                transformOrigin: "50% 50% -30px",
-                rotationY: -360,
-                rotationX: 360,
-                rotation: 360
-            }, ease: Elastic.easeInOut
-        }, 0.02, "+=1");
-
-    }
-
-    componentWillUnmount() {
-        console.log('component will unmount');
-    }
-
-    endListener = (node: HTMLElement, done: () => void) => {
-        console.log('endListener', this.props.in);
-        if (this.props.in) {
-            this.tl.play(0).eventCallback('onComplete', () => {
-                console.log('starting', 'onComplete')
-                done();
-            });
-        } else {
-            this.tl.reverse(0.8)
-                .timeScale(1.5)
-                .eventCallback('onReverseComplete', () => {
-                    console.log('removing', 'onReverseComplete')
-                    done();
-                    delete this.domRef;
-                });
-        }
-    }
-
-    render() {
-        const lines = this.props.text.split("\n");
-
-
-        return (
-          <Transition
-            in={this.props.in}
-            timeout={2000}
-            mountOnEnter={true}
-            unmountOnExit={true}
-            addEndListener={this.endListener}
-          >
-              {state => {
-                  return (
-                      <div ref={(el: HTMLDivElement) => {
-                          this.domRef = el
-                      }}>
-                          {lines.map((line, idx) => (
-                              <React.Fragment key={idx}>
-                                  <p>{line.trim()}</p>
-                              </React.Fragment>
-                          ))}
-                          <img className="test" src={this.props.image} />
-                      </div>
-                  )
-              }
-              }
-          </Transition>
-        )
-    }
-
-}
-
-
-export class FadeEl extends React.Component<{in?: boolean, content: any, target: any, remove: any}, {}> {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        //const { content, target, remove } = this.props;
-        const duration=2000;
-        return (
-            <Transition
-                in={this.props.in}
-                timeout={duration}
-                mountOnEnter={true}
-                unmountOnExit={true}
-                addEndListener={(n, done) => {
-                    TweenLite.to(n, 1, { opacity: 1, x: 0 });
-                    if (this.props.in) {
-                        TweenLite.to(n, 1, {
-                            opacity: 1,
-                            x: +100,
-                            ease: Power2.easeOut,
-                            onComplete: done });
-                    } else {
-                        TweenLite.to(n, 1, {
-                            opacity: 0,
-                            x: -100,
-                            ease: Power2.easeOut,
-                            onComplete: done
-                        });
-                    }
-                }}
-            >
-                <FadeComponent {...this.props} />
-            </Transition>
-        );
-    }
-}
-
-
-class FadeComponent extends React.Component<any,any> {
-    componentDidMount() {
-        TweenLite.to(this.refs.targetEl, 0.5, {
-            opacity: 1,
-            x: 0,
-            delay: 0.2 * this.props.target
-        });
-    }
-    render() {
-        const { content, target, remove } = this.props;
-        return (
-            <div
-                className="card my-element"
-                ref="targetEl"
-                style={{ marginTop: "10px" }}
-            >
-                <div className="card-block">
-                    {content}
-                    <button
-                        className="btn btn-danger float-right"
-                        onClick={() => remove(target)}
-                    >
-                        Delete
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
 
 export default MenuLayout;
